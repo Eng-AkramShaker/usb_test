cbc8ce9536fd87f839524d1c6fcff0f293d9ee66
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-07-30 15:24:29 +0200

    55

---
+++ build/printer/generated/source/buildConfig/release/in/mobiocean/printer/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package in.mobiocean.printer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "in.mobiocean.printer";
+  public static final String BUILD_TYPE = "release";
+}
